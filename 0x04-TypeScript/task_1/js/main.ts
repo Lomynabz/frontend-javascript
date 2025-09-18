55ba52fdad9cc16393e879ec2797999dc1cec8ff
Lomnyak Nabaala <lomnyaknab@gmail.com>
2025-09-18 15:57:36 +0300

    committed

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,14 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -30,3 +38,6 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+// Test printTeacher function
+console.log(printTeacher("John", "Doe")); // Should print: J. Doe
